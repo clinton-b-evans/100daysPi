58e6d8b192d823150685f053ef3a1f788a84fe24
Clinton Evans <study@getctrl.co.nz>
2024-01-02 06:21:10 +1300

    complete quiz game

--- Day 17/quiz_project/main.py
+++ Day 17/quiz_project/main.py
@@ -1,18 +1,15 @@
 from question_model import Question
 from data import question_data 
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
-score = 0
+quiz = QuizBrain(question_bank)
 
-for question in question_bank:
-    print(question.text)
-    question_answer = input("True or False?")
-    if question_answer == question.answer:
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect!")
-    print()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("Well done you've finished the quiz")
+print(f"Your final score was : {quiz.score} / {len(question_bank)}")
