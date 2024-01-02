c45f658358dfa195a250f0dd8f1b978214accfc3
Clinton Evans <study@getctrl.co.nz>
2024-01-03 10:11:38 +1300

    update range to 15

--- Day 18/main.py
+++ Day 18/main.py
@@ -3,7 +3,7 @@ raphael = Turtle()
 raphael.shape("arrow")
 raphael.color("green")
 count = 0
-for _ in range(30):
+for _ in range(15):
     count += 1
     if count % 2 == 0:
         raphael.penup()
