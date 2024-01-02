97d369f51280fd8b79fd5159be441f3893087916
Clinton Evans <study@getctrl.co.nz>
2024-01-03 10:29:19 +1300

    complete turtle challange

---
+++ Day 18/02_shape_turtle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+# Set up the screen and turtle. The colormode needs to be setup to use random 255 colors. 
+screen = Screen()
+screen.colormode(255)
+
+from random import randint
+raphael = Turtle()
+raphael.shape("arrow")
+count = 0
+for _ in range(3,11,1):
+    angle = int(360 / _)
+    raphael.color(randint(0,255),
+                  randint(0,255),
+                  randint(0,255),)
+    for shape in range(_):
+        raphael.forward(100)
+        raphael.left(angle)
+
+screen.exitonclick()
