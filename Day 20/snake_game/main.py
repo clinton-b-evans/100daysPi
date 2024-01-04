916f779dff514f7a177cf2a212dc976ea552cb67
Clinton Evans <study@getctrl.co.nz>
2024-01-05 06:07:05 +1300

    adjust screen animation and set snake to move right infinite loop

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -1,26 +1,28 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game / Nokia Throwback")
-snake_body = []
-snake_body_length = 3
+screen.tracer(0)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-for i in range(0, snake_body_length):
-    snake = Turtle(visible=False)
-    snake.penup()
-    snake.shape("square")
-    snake.size = 20
-    snake.color("white")
-    snake.goto(i*-20,0)
-    snake.pendown()
-    snake_body.append(snake)
-    print(snake.position())
+segments = []
 
-for i in range(0, snake_body_length):
-    snake = snake_body[i].showturtle()
- 
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
+game_is_on = True
+
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+        screen.update()
+        time.sleep(1)
 
 screen.exitonclick()
