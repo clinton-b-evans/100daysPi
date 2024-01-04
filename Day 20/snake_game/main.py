a671ae2d938398c5337e03b895145b94f90c4ae6
Clinton Evans <study@getctrl.co.nz>
2024-01-05 06:19:00 +1300

    tidy up snake class

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -15,5 +15,5 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
+    snake.move()
 screen.exitonclick()
--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 class Snake():
+    '''Creates a snake object with 3 starting segments'''
     def __init__(self):
         starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
@@ -10,3 +11,12 @@ class Snake():
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def move(self):
+        '''Moves snake forward by 20 pixels, starting from the tail'''
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.segments[0].forward(20)
