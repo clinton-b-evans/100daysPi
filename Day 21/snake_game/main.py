e8e9725a316d63c7717701e14f770183b21f6c56
Clinton Evans <study@getctrl.co.nz>
2024-01-06 06:49:26 +1300

    refactor: slice snake segment to exclude first value

--- Day 21/snake_game/main.py
+++ Day 21/snake_game/main.py
@@ -35,10 +35,8 @@ while game_is_on:
         snake.extend()
     
     # Ouroboros: Snake eats itself / game over condition.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()    
     
