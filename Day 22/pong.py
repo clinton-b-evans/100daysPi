a60f6c4923074aa13be07afe11cceb5f6ac1217c
Clinton Evans <study@getctrl.co.nz>
2024-01-07 06:52:17 +1300

    added scoreboard and point system

--- Day 22/pong.py
+++ Day 22/pong.py
@@ -1,12 +1,14 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+scoreboard = Scoreboard()
 right_paddle = Paddle((350,0))
 left_paddle = Paddle((-350,0))
 ball = Ball()
@@ -33,7 +35,14 @@ while game_is_on:
     if ball.xcor() > 320 and ball.distance(right_paddle) < 50 or ball.xcor() < -320 and ball.distance(left_paddle) < 50:
         ball.bounce_x()
 
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380:
         ball.score()
+        scoreboard.left_point()
+
+    if ball.xcor() < -380:
+        ball.score()
+        scoreboard.right_point()
+
+
 
 screen.exitonclick()
