c838283fb127ca7836566513323bf590ad2165c4
Clinton Evans <study@getctrl.co.nz>
2024-01-07 06:25:31 +1300

    refactor Ball class. Added new_x calculation to variable assignment

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -12,9 +12,9 @@ class Ball(Turtle):
         self.y_move = 10
     
     def move(self):
-        new_x = self.xcor()
-        new_y = self.ycor()
-        self.goto(new_x + self.x_move, new_y + self.y_move)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
     
     def bounce(self):
         self.y_move *= -1
