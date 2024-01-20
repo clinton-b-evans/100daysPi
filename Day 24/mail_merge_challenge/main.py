1d9b5cf0332629a6d41c9c7a3f3784d718b8b056
Clinton Evans <study@getctrl.co.nz>
2024-01-21 05:29:25 +1300

    added comments

--- Day 24/mail_merge_challenge/main.py
+++ Day 24/mail_merge_challenge/main.py
@@ -1,11 +1,14 @@
+# OPEN STARTING LETTER TEMPLATE 
 with open("Input/Letters/starting_letter.txt") as file:
     contents = file.read()
     letter_template = contents
 
+# CREATE LIST FROM INVITED NAMES
 with open("Input/Names/invited_names.txt") as file:
     content = file.read()
     invited_names = content.splitlines()
 
+# RUN LOOP FOR EACH NAME IN LIST AND CREATE INDIVIDUAL LETTER
 for name in invited_names:
     new_letter = letter_template.replace("[name]", name)
     with open(f"Output/ReadyToSend/{name}_invite.txt",mode="w") as file:
