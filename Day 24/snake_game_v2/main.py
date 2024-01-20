9355e79fb31ac93033981b66e6fdab78fe88e156
Clinton Evans <study@getctrl.co.nz>
2024-01-21 05:09:43 +1300

    update high score function to write to txt file

--- Day 24/snake_game_v2/main.py
+++ Day 24/snake_game_v2/main.py
@@ -26,7 +26,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    scoreboard
 
     # Snake eats food / collision condition.
     if snake.head.distance(food) < 15:
@@ -37,12 +36,13 @@ while game_is_on:
     # Ouroboros: Snake eats itself / game over condition.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()    
+            scoreboard.reset()
+            snake.reset()   
     
 
     # Snake hits wall / game over condition.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
 screen.exitonclick()
