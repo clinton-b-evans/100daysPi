ebfd307f6d6e29f2bdca0371649649130db6dfa5
Clinton Evans <study@getctrl.co.nz>
2024-01-22 23:13:53 +1300

    refactor code and resolve error if same answer typed twice

--- Day 25/us-states-game-start/main.py
+++ Day 25/us-states-game-start/main.py
@@ -12,14 +12,17 @@ counter = 0
 
 while len(remaining_states) > 0:
     answer_state = screen.textinput(title=f"{counter}/50 || Guess the State", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        break
     found_state = data[data.state == answer_state]
-    if len(found_state) > 0:
+    if answer_state in remaining_states:
         t = turtle.Turtle()
         t.penup()
         t.hideturtle()
         t.goto(found_state.x.values[0], found_state.y.values[0])
         t.write(answer_state)
         # data = data.drop(answer_state)
-        remaining_states.remove(answer_state)
+        remaining_states.remove(answer_state) 
         counter += 1
-screen.exitonclick()
+        new_data = pandas.DataFrame(remaining_states)
+        new_data.to_csv("Day 25/us-states-game-start/remaining_states.csv")
